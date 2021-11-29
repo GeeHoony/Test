c930cc1d45dc38710b759059ff130ba5d3342f8d
ShinJiHoon <jh.shin@hitecrcd.net>
2021-11-29 14:59:10 +0900

    Test Commit

---
+++ Window Explorer/Window Explorer/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.DriveList = New System.Windows.Forms.ImageList(Me.components)
+        Me.FileList = New System.Windows.Forms.ImageList(Me.components)
+        Me.lvFile = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.treeDisk = New System.Windows.Forms.TreeView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DriveList
+        '
+        Me.DriveList.ImageStream = CType(resources.GetObject("DriveList.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.DriveList.TransparentColor = System.Drawing.Color.Transparent
+        Me.DriveList.Images.SetKeyName(0, "0.JPG")
+        Me.DriveList.Images.SetKeyName(1, "1.JPG")
+        Me.DriveList.Images.SetKeyName(2, "2.JPG")
+        Me.DriveList.Images.SetKeyName(3, "3.JPG")
+        Me.DriveList.Images.SetKeyName(4, "4.JPG")
+        Me.DriveList.Images.SetKeyName(5, "5.JPG")
+        '
+        'FileList
+        '
+        Me.FileList.ImageStream = CType(resources.GetObject("FileList.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.FileList.TransparentColor = System.Drawing.Color.Transparent
+        Me.FileList.Images.SetKeyName(0, "bmp.bmp")
+        Me.FileList.Images.SetKeyName(1, "com.bmp")
+        Me.FileList.Images.SetKeyName(2, "doc.bmp")
+        Me.FileList.Images.SetKeyName(3, "etc.bmp")
+        Me.FileList.Images.SetKeyName(4, "exe.bmp")
+        Me.FileList.Images.SetKeyName(5, "Folder.bmp")
+        Me.FileList.Images.SetKeyName(6, "gif.bmp")
+        Me.FileList.Images.SetKeyName(7, "htm.bmp")
+        Me.FileList.Images.SetKeyName(8, "html.bmp")
+        Me.FileList.Images.SetKeyName(9, "hwp.bmp")
+        Me.FileList.Images.SetKeyName(10, "jpg.bmp")
+        Me.FileList.Images.SetKeyName(11, "mp3.bmp")
+        Me.FileList.Images.SetKeyName(12, "mpg.bmp")
+        Me.FileList.Images.SetKeyName(13, "ra.bmp")
+        Me.FileList.Images.SetKeyName(14, "rar.bmp")
+        Me.FileList.Images.SetKeyName(15, "txt.bmp")
+        Me.FileList.Images.SetKeyName(16, "zip.bmp")
+        '
+        'lvFile
+        '
+        Me.lvFile.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.lvFile.FullRowSelect = True
+        Me.lvFile.Location = New System.Drawing.Point(258, 12)
+        Me.lvFile.Name = "lvFile"
+        Me.lvFile.Size = New System.Drawing.Size(541, 449)
+        Me.lvFile.SmallImageList = Me.FileList
+        Me.lvFile.TabIndex = 0
+        Me.lvFile.UseCompatibleStateImageBehavior = False
+        Me.lvFile.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "파일 이름"
+        Me.ColumnHeader1.Width = 140
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "크기"
+        Me.ColumnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader2.Width = 80
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "파일 폴더"
+        Me.ColumnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader3.Width = 110
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "최근 수정 날짜"
+        Me.ColumnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader4.Width = 170
+        '
+        'treeDisk
+        '
+        Me.treeDisk.ImageIndex = 0
+        Me.treeDisk.ImageList = Me.DriveList
+        Me.treeDisk.Location = New System.Drawing.Point(12, 12)
+        Me.treeDisk.Name = "treeDisk"
+        Me.treeDisk.SelectedImageIndex = 0
+        Me.treeDisk.Size = New System.Drawing.Size(230, 449)
+        Me.treeDisk.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.treeDisk)
+        Me.Panel1.Controls.Add(Me.lvFile)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(931, 541)
+        Me.Panel1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(821, 489)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "윈도우 탐색기"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DriveList As ImageList
+    Friend WithEvents FileList As ImageList
+    Friend WithEvents lvFile As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+    Friend WithEvents treeDisk As TreeView
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ Window Explorer/Window Explorer/Form1.vb
@@ -0,0 +1,141 @@
+﻿Imports System.IO
+Imports System.Management
+
+Public Class Form1
+
+    Dim path As String = "C:\\"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TreeView()
+    End Sub
+
+    Private Sub TreeView()
+        Dim RootNode, SubNode As TreeNode
+        Dim ImageIndex = 5
+        Dim selectIndex = 5
+
+        'const Unknown = 0
+        'const NoRootDirectory = 1
+        Const RemovableDisk = 2
+        Const LocalDisk = 3
+        'const NetworkDrive = 4
+        Const CompactDisc = 5
+        Const RAMDisk = 6
+
+
+        Dim query = New ManagementObjectSearcher("SELECT * From Win32_LogicalDisk")
+        Dim queryCollection = query.Get()
+
+        Me.treeDisk.BeginUpdate()
+        RootNode = New TreeNode("내 컴퓨터", ImageIndex, selectIndex)
+        Me.treeDisk.Nodes.Add(RootNode)
+
+        For Each drive In queryCollection
+            Select Case Convert.ToInt32(drive("DriveType").ToString())
+                Case RemovableDisk
+                    ImageIndex = 0
+                    selectIndex = 0
+                Case LocalDisk
+                    ImageIndex = 1
+                    selectIndex = 1
+                Case CompactDisc
+                    ImageIndex = 2
+                    selectIndex = 2
+                Case RAMDisk
+                    ImageIndex = 3
+                    selectIndex = 3
+                Case Else
+                    ImageIndex = 0
+                    selectIndex = 0
+            End Select
+
+            SubNode = New TreeNode("(" + drive("Name").ToString() + ")", ImageIndex, selectIndex)
+            Me.treeDisk.Nodes(0).Nodes.Add(SubNode)
+        Next
+        Me.treeDisk.EndUpdate()
+    End Sub
+
+    Private Sub View(ByVal dr As String)
+        Me.Text = path
+        Me.lvFile.Items.Clear()
+        Dim imageNum = 15
+        Try
+            If (Directory.Exists(dr) = False) Then
+                MessageBox.Show(dr + " 드라이브는 읽을 수 없습니다.", "디스크 삽입", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Else
+                Dim di = New DirectoryInfo(dr)
+
+                For Each d In di.GetDirectories()
+                    Dim lvItem = New ListViewItem(New String() {d.Name, "", "폴더", d.LastWriteTime.ToShortDateString()}, 16)
+                    Me.lvFile.Items.Add(lvItem)
+                Next
+
+                For Each d In di.GetFiles()
+                    Dim Str = d.Extension
+                    Str = Str.ToLower()
+                    Select Case Str.ToString()
+                        Case ".exe"
+                            imageNum = 0
+                        Case ".com"
+                            imageNum = 1
+                        Case ".txt"
+                            imageNum = 2
+                        Case ".bmp"
+                            imageNum = 3
+                        Case ".doc"
+                            imageNum = 4
+                        Case ".gif"
+                            imageNum = 5
+                        Case ".htm"
+                            imageNum = 6
+                        Case ".html"
+                            imageNum = 7
+                        Case ".hwp"
+                            imageNum = 8
+                        Case ".jpg"
+                            imageNum = 9
+                        Case ".mp3"
+                            imageNum = 10
+                        Case ".mpg"
+                            imageNum = 11
+                        Case ".ra"
+                            imageNum = 12
+                        Case ".rar"
+                            imageNum = 13
+                        Case ".zip"
+                            imageNum = 14
+                        Case Else
+                            imageNum = 15
+                    End Select
+                    Dim lvi = New ListViewItem(New String() {d.Name, d.Length.ToString(), d.Extension, d.LastWriteTime.ToShortDateString()}, imageNum)
+                    Me.lvFile.Items.Add(lvi)
+                Next
+            End If
+        Catch
+            MessageBox.Show("오류 메시지 입니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub treeDisk_AfterSelect(ByVal sender As System.Object, ByVal e As System.Windows.Forms.TreeViewEventArgs) Handles treeDisk.AfterSelect
+        If Me.treeDisk.SelectedNode.Text <> "내 컴퓨터" Then
+            Dim str = Me.treeDisk.SelectedNode.Text.Substring(1, 1)
+            path = str + ":\" 'tree view 컨트롤에 현재 선택되어 있는 트리 노드를 가져오거나 설정합니다.
+            View(path) 'view 메서드 호출
+        End If
+    End Sub
+
+    Private Sub lvFile_DoubleClick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lvFile.DoubleClick
+        If (Me.lvFile.SelectedItems.Count <> 0) Then
+            If (Me.lvFile.SelectedItems(0).SubItems(2).Text = "폴더") Then
+                path = path + Me.lvFile.SelectedItems(0).SubItems(0).Text + "\"
+                View(path)
+            Else
+                Dim myProcess = New Process()
+                myProcess.StartInfo.FileName = path + Me.lvFile.SelectedItems(0).SubItems(0).Text
+                myProcess.Start()
+            End If
+        Else
+            MessageBox.Show("디렉토리를 선택해 주세요.", "디렉토리 선택", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+End Class
