2ab7b3fb7a565fd54d6387ebbf60ebae7dc5dd75
ShinJiHoon <jh.shin@hitecrcd.net>
2021-11-29 16:41:20 +0900

    Test Commit

---
+++ System Memory/System Memory/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.plBar = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'plBar
+        '
+        Me.plBar.BackColor = System.Drawing.Color.White
+        Me.plBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.plBar.Location = New System.Drawing.Point(31, 31)
+        Me.plBar.Name = "plBar"
+        Me.plBar.Size = New System.Drawing.Size(395, 27)
+        Me.plBar.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(463, 88)
+        Me.Controls.Add(Me.plBar)
+        Me.Name = "Form1"
+        Me.Text = "메모리 체크"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents plBar As Panel
+End Class
---
+++ System Memory/System Memory/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Threading
+Imports System.Diagnostics
+
+Public Class Form1
+
+    Dim oCPU As PerformanceCounter = New PerformanceCounter("Processor", "% Processor Time", "_Total") '시스템 성능 카운터
+    Dim bExit As Boolean = False '실시간 체크를 위한 While 조건
+    Dim iCPU As Integer = 0 'cpu 초기 사용률
+    Dim F As Font = New Font("굴림", 9) '폰트 모양
+    Dim checkThread As Thread '스레드 개체 생성
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        checkThread = New Thread(AddressOf getCPU_Info)
+        checkThread.Start() 'checkThread 스레드 프로세스 시작
+    End Sub
+
+    Private Sub getCPU_Info()
+        While Not bExit
+            iCPU = oCPU.NextValue()
+            Me.Text = "CPU 사용: " + iCPU.ToString() + "%"
+            iCPU = iCPU * 3
+            plBar.Invalidate()
+            Thread.Sleep(1000)
+        End While
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        Dim G As Graphics = e.Graphics
+
+        If (iCPU <= 60) Then
+            G.FillRectangle(Brushes.BlanchedAlmond, 0, 0, iCPU, plBar.Height)
+        ElseIf (iCPU <= 120) Then
+            G.FillRectangle(Brushes.Wheat, 0, 0, iCPU, plBar.Height)
+        ElseIf (iCPU <= 180) Then
+            G.FillRectangle(Brushes.NavajoWhite, 0, 0, iCPU, plBar.Height)
+        ElseIf (iCPU <= 240) Then
+            G.FillRectangle(Brushes.Orange, 0, 0, iCPU, plBar.Height)
+        Else
+            G.FillRectangle(Brushes.DarkOrange, 0, 0, iCPU, plBar.Height)
+        End If
+
+        iCPU = iCPU / 3
+
+        G.DrawString(iCPU.ToString() + " %", F, Brushes.DarkRed, plBar.Width / 2 - 17, plBar.Height / 4)
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        checkThread.Abort()
+    End Sub
+End Class
