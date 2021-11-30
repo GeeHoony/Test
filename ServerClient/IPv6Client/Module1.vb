7c287793a9f4fab2fa42dac2c1ced96f1655259c
ShinJiHoon <jh.shin@hitecrcd.net>
2021-11-30 16:30:58 +0900

    Test Commit

---
+++ ServerClient/IPv6Client/Module1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Threading
+
+Module Module1
+
+    Sub Main()
+        IPv6Client()
+    End Sub
+
+    Sub IPv6Client()
+        Const PORT = 2011
+        Const IPv6_ADDR = "fe80::f0a2:a5dd:11b5:e7a8%4"
+
+        Dim ipa = IPAddress.Parse(IPv6_ADDR)
+        Dim ipe = New IPEndPoint(ipa, PORT)
+        Dim SockCon = New Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp)
+        SockCon.Connect(ipe)
+
+        Dim d = System.Text.ASCIIEncoding.Default.GetBytes("클라이언트가 보냅니다." + DateTime.Now)
+        For x = 0 To 9
+            Console.WriteLine("송신: " + System.Text.ASCIIEncoding.Default.GetString(d))
+            SockCon.Send(d)
+            Thread.Sleep(1000)
+        Next x
+        SockCon.Close()
+    End Sub
+End Module
---
+++ ServerClient/ServerClient/Module1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+
+Module Module1
+
+    Sub Main()
+        IPv6Server()
+    End Sub
+
+    Sub IPv6Server()
+        Const PORT = 2011
+
+        Dim SocListener = New Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp)
+        SocListener.Bind(New IPEndPoint(IPAddress.IPv6Any, PORT))
+        SocListener.Listen(1)
+        Console.WriteLine("접속을 기다리고 있습니다...")
+        Dim Socket = SocListener.Accept()
+        SocListener.Close()
+        Console.WriteLine("클라이언트가 접속하였습니다...")
+
+        Dim d(47) As Byte
+        While (Socket.Receive(d) > 0)
+            System.Console.WriteLine("수신 : " + System.Text.ASCIIEncoding.Default.GetString(d, 0 ,47))
+        End While
+        Socket.Close()
+
+    End Sub
+
+End Module
