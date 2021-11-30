b1f75247be56bb809feece788ceb1b13321c0c1e
ShinJiHoon <jh.shin@hitecrcd.net>
2021-11-30 09:51:57 +0900

    Test Commit

---
+++ XML FIle Information Explorer/XML FIle Information Explorer/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lvFile = New System.Windows.Forms.ListView()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.chName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'lvFile
+        '
+        Me.lvFile.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.chName})
+        Me.lvFile.GridLines = True
+        Me.lvFile.Location = New System.Drawing.Point(12, 21)
+        Me.lvFile.Name = "lvFile"
+        Me.lvFile.Size = New System.Drawing.Size(287, 417)
+        Me.lvFile.TabIndex = 0
+        Me.lvFile.UseCompatibleStateImageBehavior = False
+        Me.lvFile.View = System.Windows.Forms.View.Details
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(339, 21)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 1
+        Me.btnSearch.Text = "검색"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'chName
+        '
+        Me.chName.Text = "이름"
+        Me.chName.Width = 200
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 450)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.lvFile)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "XML 파일 검색"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lvFile As ListView
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents chName As ColumnHeader
+End Class
---
+++ XML FIle Information Explorer/XML FIle Information Explorer/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Net
+Imports System.Xml
+Imports System.IO
+
+Public Class Form1
+
+    Const headerURL = "http://localhost/"
+    Dim Nullable As XmlAttribute
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Me.lvFile.Items.Clear()
+
+        Dim wc = New WebClient()
+        Dim buffer = wc.DownloadString(String.Format("{0}WebXml.xml", headerURL))
+        wc.Dispose()
+
+        Dim sr As StringReader = New StringReader(buffer)
+        Dim doc As XmlDocument = New XmlDocument()
+        doc.Load(sr)
+        sr.Close()
+
+        Dim forecastNodes As XmlNodeList = doc.SelectNodes("xml_reply/human/human_entry")
+
+        For Each node As XmlNode In forecastNodes
+            Me.lvFile.Items.Add(New ListViewItem(New String() {GetNodeValue(node, "title")}))
+        Next
+    End Sub
+
+    Private Function GetNodeValue(ByVal parent As XmlNode, ByVal name As String) As String
+        Try
+            Dim attr As XmlAttribute = parent.SelectSingleNode(name).Attributes("name")
+            If attr.Value <> " " Then
+                Return attr.Value
+            End If
+
+            Return " "
+        Catch
+            Return " "
+        End Try
+    End Function
+
+End Class
